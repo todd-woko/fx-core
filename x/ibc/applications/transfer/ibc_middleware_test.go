e95c8f2d0f2ab9a8b3ca49255da30af59b03e775
fx0x55 <dennis@wokoworks.com>
2022-08-08 18:16:43 +0800

    test: FungibleTokenPacketData unmarshalJSON test

--- x/ibc/applications/transfer/ibc_middleware_test.go
+++ x/ibc/applications/transfer/ibc_middleware_test.go
@@ -5,6 +5,8 @@ import (
 	"math"
 	"testing"
 
+	"github.com/functionx/fx-core/v2/x/ibc/applications/transfer/types"
+
 	"github.com/functionx/fx-core/v2/x/ibc/applications/transfer"
 
 	transfertypes "github.com/cosmos/ibc-go/v3/modules/apps/transfer/types"
@@ -384,6 +386,91 @@ func TestGetDenomByIBCPacket(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			actualValue := transfer.GetDenomByIBCPacket(tc.sourcePort, tc.sourceChannel, tc.destPort, tc.destChannel, tc.packetDenom)
 			require.EqualValues(t, tc.expDenom, actualValue)
+		})
+	}
+}
+
+func TestUnmarshalJSON(t *testing.T) {
+	testCases := []struct {
+		name   string
+		data   []byte
+		pass   bool
+		expErr error
+		exp    types.FungibleTokenPacketData
+	}{
+		{
+			name:   "fx transfer packet - no router",
+			data:   types.NewFungibleTokenPacketData("FX", "100", "Add1", "Add2", "", "0").GetBytes(),
+			pass:   true,
+			expErr: nil,
+			exp: types.FungibleTokenPacketData{
+				Denom:    "FX",
+				Amount:   "100",
+				Sender:   "Add1",
+				Receiver: "Add2",
+				Router:   "",
+				Fee:      "0",
+			},
+		},
+		{
+			name:   "fx transfer packet - router with 0fee",
+			data:   types.NewFungibleTokenPacketData("FX", "100", "Add1", "Add2", "router", "0").GetBytes(),
+			pass:   true,
+			expErr: nil,
+			exp: types.FungibleTokenPacketData{
+				Denom:    "FX",
+				Amount:   "100",
+				Sender:   "Add1",
+				Receiver: "Add2",
+				Router:   "router",
+				Fee:      "0",
+			},
+		},
+		{
+			name:   "fx transfer packet - router with empty fee",
+			data:   types.NewFungibleTokenPacketData("FX", "100", "Add1", "Add2", "router", "").GetBytes(),
+			pass:   true,
+			expErr: nil,
+			exp: types.FungibleTokenPacketData{
+				Denom:    "FX",
+				Amount:   "100",
+				Sender:   "Add1",
+				Receiver: "Add2",
+				Router:   "router",
+				Fee:      "",
+			},
+		},
+		{
+			name:   "ibc transfer packet",
+			data:   transfertypes.NewFungibleTokenPacketData("FX", "100", "Add1", "Add2").GetBytes(),
+			pass:   true,
+			expErr: nil,
+			exp: types.FungibleTokenPacketData{
+				Denom:    "FX",
+				Amount:   "100",
+				Sender:   "Add1",
+				Receiver: "Add2",
+				Router:   "",
+				Fee:      "",
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			var packet types.FungibleTokenPacketData
+			err := types.ModuleCdc.UnmarshalJSON(testCase.data, &packet)
+			if testCase.pass {
+				require.NoError(t, err)
+				require.EqualValues(t, testCase.exp, packet)
+				require.EqualValues(t, testCase.exp.GetDenom(), packet.GetDenom())
+				require.EqualValues(t, testCase.exp.GetAmount(), packet.GetAmount())
+				require.EqualValues(t, testCase.exp.GetSender(), packet.GetSender())
+				require.EqualValues(t, testCase.exp.GetReceiver(), packet.GetReceiver())
+				require.EqualValues(t, testCase.exp.GetRouter(), packet.GetRouter())
+				require.EqualValues(t, testCase.exp.GetFee(), packet.GetFee())
+			} else {
+				require.ErrorIs(t, err, testCase.expErr)
+			}
 		})
 	}
 }
